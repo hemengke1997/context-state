87931352547c8067dfdf4021f17fd15d02e670e0
Minko <49073282+hemengke1997@users.noreply.github.com>
2022-05-20 18:07:58 +0800

    fix: external

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'React-dom',
+          'react-dom': 'ReactDom',
         },
       },
     },
