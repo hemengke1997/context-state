1f07a66286878735bfbe29d0588c1b98551f60fc
hemengke <23536175@qq.com>
2025-03-11 18:30:30 +0800

    chore: lint

--- test/basic.test.tsx
+++ test/basic.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { cleanup, fireEvent, render } from '@testing-library/react'
+import React from 'react'
 import { afterEach, describe, expect, it } from 'vitest'
 import { createStore } from '../src'
 
